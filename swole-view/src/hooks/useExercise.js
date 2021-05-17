2336248b57b007daec6b7408a55c29096833dde9
Nate <nathan.rosandich@gmail.com>
2021-05-17 21:10:59 +1200

    add more exercise

--- swole-view/src/hooks/useExercise.js
+++ swole-view/src/hooks/useExercise.js
@@ -7,8 +7,8 @@ export const useExercise = () => {
     const currentIndex = ref(0);
     const total = ref(0);
 
-    const exercises = {
-        plank: {
+    const exercises = [
+        {
             name: 'Plank',
             image: '../img/exercises/plank.png',
             desc: 'Lay on the floor with your elbows under your shoulders, hands flat on the floor and core engaged. Keeping your forearms and knees on the floor slowly raise yourself upwards until your body is in a straight line from your knees to your head. Hold the position for as long as you can.',
@@ -17,7 +17,7 @@ export const useExercise = () => {
             3: '30 sec',
             4: '40 sec'
         },
-        pushup: {
+        {
             name: 'Push ups',
             image: '../img/exercises/pushup.png',
             desc: 'Get down on all fours, placing your hands slightly wider than your shoulders. Straighten your arms and legs. Lower your body until your chest nearly touches the floor. Pause, then push yourself back up. Repeat.',
@@ -26,7 +26,7 @@ export const useExercise = () => {
             3: '30',
             4: '40'
         },
-        situp: {
+        {
             name: 'Situps',
             image: '../img/exercises/situp.png',
             desc: 'Lie down on your back. Bend your legs and stabilize your lower body. Cross your hands to opposite shoulders, or place them behind your ears without pulling on your neck. Lift your head and shoulder blades from the ground. Exhale as you rise. Lower, returning to your starting point. Inhale as you lower.',
@@ -35,7 +35,7 @@ export const useExercise = () => {
             3: '60',
             4: '80'
         },
-        burpee: {
+        {
             name: 'Burpees',
             image: '../img/exercises/burpee.png',
             desc: 'Stand with your feet shoulder-width apart and your arms by your sides. Lower into a squat position and place your hands on the floor. Kick or step your legs back into a plank position. Jump or step your legs forward to return to a squat position. Return to the standing position.',
@@ -43,61 +43,36 @@ export const useExercise = () => {
             2: '15',
             3: '20',
             4: '25'
+        },
+        {
+            name: 'Lunges',
+            image: '../img/exercises/lunges.png',
+            desc: 'Start by standing up tall. Step forward with one foot until your leg reaches a 90-degree angle. Lift your front lunging leg to return to the starting position. Repeat 10 to 12 reps on one leg, or switch off between legs until youve totaled 10 to 12 reps per leg.',
+            1: '10',
+            2: '15',
+            3: '20',
+            4: '25'
+        },
+        {
+            name: 'Leg Raises',
+            image: '../img/exercises/leg-raise.png',
+            desc: 'Lie on your back, legs straight and together. Keep your legs straight and lift them all the way up to the ceiling until your butt comes off the floor. Slowly lower your legs back down till theyre just above the floor. Hold for a moment. Raise your legs back up. Repeat.',
+            1: '10',
+            2: '15',
+            3: '20',
+            4: '25'
+        },
+        {
+            name: 'Squats',
+            image: '../img/exercises/squat.png',
+            desc: 'Stand straight with feet hip-width apart. Tighten your stomach muscles. Lower down, as if sitting in an invisible chair. Straighten your legs to lift back up. Repeat the movement.',
+            1: '10',
+            2: '15',
+            3: '20',
+            4: '25'
         }
-    };
+    ];
     //pics https://thenounproject.com/creativestall/collection/pictograms-line-icons/
-
-    const days = {
-        0: {
-            name: 'Sunday',
-            exercises: [
-                'plank',
-                'pushup'
-            ]
-        },
-        1: {
-            name: 'Monday',
-            exercises: [
-                'situp',
-                'pushup'
-            ]
-        },
-        2: {
-            name: 'Tuesday',
-            exercises: [
-                'plank',
-                'situp'
-            ]
-        },
-        3: {
-            name: 'Wednesday',
-            exercises: [
-                'burpee',
-                'pushup'
-            ]
-        },
-        4: {
-            name: 'Thursday',
-            exercises: [
-                'plank',
-                'burpee'
-            ]
-        },
-        5: {
-            name: 'Friday',
-            exercises: [
-                'plank',
-                'pushup'
-            ]
-        },
-        6: {
-            name: 'Saturday',
-            exercises: [
-                'situp',
-                'burpee'
-            ]
-        },
-    }
 
     const reset = () => {
         currentIndex.value = 0;
@@ -105,18 +80,17 @@ export const useExercise = () => {
         currentExercise.value = userExercises.value[0];
     }
 
-    const setExercises = ({ level = 1 } = {}) => {
-        const day = new Date().getDay();
-        const exerciseDay = days[day];
-        userExercises.value = exerciseDay.exercises.map(type => {
-            const exercise = exercises[type];
+    const setExercises = ({ level = 1, numExercises = 4 } = {}) => {
+        const shuffledExericses = exercises.sort(() => 0.5 - Math.random());
+        const randomExericses = shuffledExericses.slice(0, numExercises);
+        userExercises.value = randomExericses.map(randomExericse => {
             return {
-                name: exercise.name,
-                desc: exercise.desc,
-                image: exercise.image,
-                amount: exercise[level],
+                name: randomExericse.name,
+                desc: randomExericse.desc,
+                image: randomExericse.image,
+                amount: randomExericse[level],
             };
-        })
+        });
         reset();
     }
     setExercises();
