1f5835a33c3e79808e5a9ab5e7d70b437a1a5857
Nate <nathan.rosandich@gmail.com>
2021-05-18 20:47:49 +1200

    add 2 new exercises

--- swole-view/src/hooks/useExercise.js
+++ swole-view/src/hooks/useExercise.js
@@ -23,73 +23,91 @@ export const useExercise = () => {
             name: 'Plank',
             image: '../img/exercises/plank.png',
             desc: 'Lay on the floor with your elbows under your shoulders, hands flat on the floor and core engaged. Keeping your forearms and knees on the floor slowly raise yourself upwards until your body is in a straight line from your knees to your head. Hold the position for as long as you can.',
-            1: '10 sec',
-            2: '20 sec',
-            3: '30 sec',
-            4: '40 sec'
+            1: '20 sec',
+            2: '30 sec',
+            3: '40 sec',
+            4: '50 sec'
         },
         {
             name: 'Push ups',
             image: '../img/exercises/pushup.png',
             desc: 'Get down on all fours, placing your hands slightly wider than your shoulders. Straighten your arms and legs. Lower your body until your chest nearly touches the floor. Pause, then push yourself back up. Repeat.',
-            1: '10',
-            2: '20',
-            3: '30',
-            4: '40'
+            1: '20',
+            2: '30',
+            3: '40',
+            4: '50'
         },
         {
             name: 'Situps',
             image: '../img/exercises/situp.png',
             desc: 'Lie down on your back. Bend your legs and stabilize your lower body. Cross your hands to opposite shoulders, or place them behind your ears without pulling on your neck. Lift your head and shoulder blades from the ground. Exhale as you rise. Lower, returning to your starting point. Inhale as you lower.',
-            1: '20',
-            2: '40',
-            3: '60',
-            4: '80'
+            1: '40',
+            2: '60',
+            3: '80',
+            4: '100'
         },
         {
             name: 'Burpees',
             image: '../img/exercises/burpee.png',
             desc: 'Stand with your feet shoulder-width apart and your arms by your sides. Lower into a squat position and place your hands on the floor. Kick or step your legs back into a plank position. Jump or step your legs forward to return to a squat position. Return to the standing position.',
-            1: '10',
-            2: '15',
-            3: '20',
-            4: '25'
+            1: '15',
+            2: '20',
+            3: '25',
+            4: '30'
         },
         {
             name: 'Lunges',
             image: '../img/exercises/lunges.png',
             desc: 'Start by standing up tall. Step forward with one foot until your leg reaches a 90-degree angle. Lift your front lunging leg to return to the starting position. Repeat 10 to 12 reps on one leg, or switch off between legs until youve totaled 10 to 12 reps per leg.',
-            1: '10',
-            2: '15',
-            3: '20',
-            4: '25'
+            1: '15',
+            2: '20',
+            3: '25',
+            4: '30'
         },
         {
             name: 'Leg Raises',
             image: '../img/exercises/leg-raise.png',
             desc: 'Lie on your back, legs straight and together. Keep your legs straight and lift them all the way up to the ceiling until your butt comes off the floor. Slowly lower your legs back down till theyre just above the floor. Hold for a moment. Raise your legs back up. Repeat.',
-            1: '10',
-            2: '15',
-            3: '20',
-            4: '25'
+            1: '15',
+            2: '20',
+            3: '25',
+            4: '30'
         },
         {
             name: 'Squats',
             image: '../img/exercises/squat.png',
             desc: 'Stand straight with feet hip-width apart. Tighten your stomach muscles. Lower down, as if sitting in an invisible chair. Straighten your legs to lift back up. Repeat the movement.',
-            1: '10',
-            2: '15',
-            3: '20',
-            4: '25'
+            1: '15',
+            2: '20',
+            3: '25',
+            4: '30'
         },
         {
             name: 'Russian twist',
             image: '../img/exercises/twist.png',
             desc: 'Sit on the floor and bring your legs out straight. Lean back slightly so your torso and legs form a V-like shape, bracing your abdominal wall to engage your core. Balancing here, twist your torso from side to side without moving your legs.',
-            1: '10',
-            2: '15',
-            3: '20',
-            4: '25'
+            1: '15',
+            2: '20',
+            3: '25',
+            4: '30'
+        },
+        {
+            name: 'Arm Curls',
+            image: '../img/exercises/curl.png',
+            desc: 'Start standing with a dumbbell in each hand. Your elbows should rest at your sides and your forearms should extend out in front of your body. Bring the dumbbells all the way up to your shoulders by bending your elbows. Once at the top, hold for a second by squeezing the muscle. Reverse the curl slowly and repeat.',
+            1: '15',
+            2: '20',
+            3: '25',
+            4: '30'
+        },
+        {
+            name: 'Shoulder press',
+            image: '../img/exercises/shoulder-press.png',
+            desc: 'Hold the dumbbells by your shoulders with your palms facing forwards and your elbows out to the sides and bent at a 90° angle. Without leaning back, extend through your elbows to press the weights above your head. Then slowly return to the starting position.',
+            1: '15',
+            2: '20',
+            3: '25',
+            4: '30'
         }
     ];
     //pics https://thenounproject.com/creativestall/collection/pictograms-line-icons/
