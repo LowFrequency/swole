f767bcf5b6433eeb87ea7cc5b2654c46bbf476f1
Nate <nathan.rosandich@gmail.com>
2021-05-18 21:59:08 +1200

    finish styling

--- swole-view/src/views/Finish.vue
+++ swole-view/src/views/Finish.vue
@@ -8,44 +8,41 @@
         class="px-6 text-white text-center rounded-lg border border-gray-300 mb-10 w-11/12 py-2 lg:w-4/5 lg:py-16"
       >
         <h5 class="font-bold text-2xl lg:py-8 py-2">
-          Great work, hope you are feeling Swole
+          Fuck yeah, you smashed
         </h5>
-        <h5 class="font-bold text-2xl lg:py-8 py-2">You completed</h5>
         <div class="flex flex-col">
           <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
               <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table class="min-w-full divide-y divide-gray-200">
-                  <thead class="bg-gray-50">
+                <table class="min-w-full divide-y divide-gray-200 text-gray-300">
+                  <thead class="">
                     <tr>
                       <th
                         scope="col"
-                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
                       >
                         Exercise
                       </th>
                       <th
                         scope="col"
-                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
                       >
                         Amount
                       </th>
                     </tr>
                   </thead>
                   <tbody>
-                    <!-- Odd row -->
                     <tr
                       v-for="userExercise in userExercises"
                       :key="userExercise.name"
-                      class="bg-white"
                     >
                       <td
-                        class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
+                        class="px-6 py-4 whitespace-nowrap text-sm font-medium"
                       >
                         {{ userExercise.name }}
                       </td>
-                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {{ userExercise.amount }}
+                      <td class="px-6 py-4 whitespace-nowrap text-sm">
+                        x{{ userExercise.amount }}
                       </td>
                     </tr>
                   </tbody>
