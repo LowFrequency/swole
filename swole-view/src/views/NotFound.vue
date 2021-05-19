8ad2a5bd85be972123a167dbb0ceedacad82f572
Nate <nathan.rosandich@gmail.com>
2021-05-19 21:06:19 +1200

    styling

--- swole-view/src/views/NotFound.vue
+++ swole-view/src/views/NotFound.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="not-found">
-    <h1>Oops, it looks like the page you're looking for doesn't exist.</h1>
+  <div class="min-h-full lg:h-full h-screen bg-gray-900">
+    <swole-header></swole-header>
+    <div
+      class="flex flex-col items-start items-center justify-center w-full w-full p-2 px-4 lg:p-12"
+    >
+      <div
+        class="px-6 text-white text-center rounded-lg border border-gray-300 mb-10 w-11/12 py-2 lg:w-4/5 lg:py-16"
+      >
+        <h5 class="font-bold text-2xl lg:py-8 py-2">
+          Oops, it looks like the page you're looking for doesn't exist.
+        </h5>
+        <div class="w-100 border-b border-gray-100" />
+        <button
+          @click="home"
+          class="uppercase border border-gray-100 text-center text-sm mt-10 px-12 py-2 rounded-md font-bold bg-primary-very-light text-primary-blue"
+        >
+          Home
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SwoleHeader from "@/components/SwoleHeader";
+import { useRouter } from "vue-router";
+
 export default {
   name: "NotFound",
+  components: {
+    SwoleHeader,
+  },
+  setup() {
+    const router = useRouter();
+
+    const home = () => {
+      router.push("/");
+    };
+    return {
+      home,
+    };
+  },
 };
 </script>
