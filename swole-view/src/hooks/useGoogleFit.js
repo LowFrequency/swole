13cb977da52e32357ace5f1697c392e4b4a2fc63
Nate <nathan.rosandich@gmail.com>
2021-06-01 22:13:47 +1200

    update the send it function to include data sourcce and points

--- swole-view/src/hooks/useGoogleFit.js
+++ swole-view/src/hooks/useGoogleFit.js
@@ -1,10 +1,11 @@
 import { ref } from "vue";
 import { getCurrentInstance } from "vue";
-import { stringToSlug } from "@/utils";
-import { addSession } from "@/services/googleFit";
+import { stringToSlug, checkArrayOfObjectsByKey } from "@/utils";
+import { addSession, addData, getDataSource, addDataSource } from "@/services/googleFit";
 
 const accessToken = ref(JSON.parse(localStorage.getItem("accessToken")));
 const googleUser = ref(JSON.parse(localStorage.getItem("googleUser")));
+const dataStreamId = ref("derived:com.google.activity.segment:257811618614:Swole");
 
 export const useGoogleFit = () => {
 
@@ -48,18 +49,37 @@ export const useGoogleFit = () => {
 
     const sendIt = async ({ start = null, finish = null, title = null } = {}) => {
         try {
+            //Check auth and ask if needed
             await connect();
 
-            //const datasource = process.env.VUE_APP_GOOGLE_APP_DATASOURCE;
-            const id = stringToSlug({ string: `swole-${title}-${start}` });
+            //Next steps are based on https://developers.google.com/fit/rest/v1/workout
             const baseUrl = `https://www.googleapis.com/fitness/v1/users/me/`;
 
-            //const dataResponse = await addData({ baseUrl, accessToken: accessToken.value, datasource, start, finish });
-            //console.log({ dataResponse })
-            //alert(`Google Fit Data ${id} added; response: ${JSON.stringify(dataResponse)}`);
-            const sessionResponse = await addSession({ baseUrl, accessToken: accessToken.value, id, start, finish, title });
-            console.log({ sessionResponse })
-            alert(`Google Fit Session ${id} added; response: ${JSON.stringify(sessionResponse)}`);
+            //First we need to check that the data source is avialable to this person
+            const dataSourceResponse = await getDataSource({ baseUrl, accessToken: accessToken.value });
+            console.log({ dataSourceResponse })
+            const dataSourceCheck = checkArrayOfObjectsByKey({ checkArray: dataSourceResponse.dataSource, key: 'dataStreamId', value: dataStreamId.value });
+            console.log({ dataSourceCheck })
+
+            //If not avalable then we add a new one and update internal data sourcce
+            if (dataSourceCheck === undefined) {
+                const newdataSourceResponse = await addDataSource({ baseUrl, accessToken: accessToken.value });
+                dataStreamId.value = newdataSourceResponse.dataStreamId;
+            }
+
+            //Now we create the actual data points
+            const id = stringToSlug({ string: `swole-${title}-${start}` });
+            const dataResponse = await addData({ baseUrl, accessToken: accessToken.value, datasource: dataStreamId.value, start, finish });
+            console.log({ dataResponse })
+            alert(`Google Fit Data ${id} added; response: ${JSON.stringify(dataResponse)}`);
+
+            //Then as long as no issue adding the above data point we add the session
+            if (!dataResponse?.error) {
+                const sessionResponse = await addSession({ baseUrl, accessToken: accessToken.value, id, start, finish, title });
+                console.log({ sessionResponse })
+                alert(`Google Fit Session ${id} added; response: ${JSON.stringify(sessionResponse)}`);
+            }
+
         } catch (err) {
             alert(`Google Fit error; response: ${JSON.stringify(err)}`);
             console.log({ err });
