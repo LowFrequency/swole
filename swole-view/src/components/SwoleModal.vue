9a92e9cb6dbcb74862b8d87b4fd682df48019295
Nate <nathan.rosandich@gmail.com>
2021-06-17 22:10:11 +1200

    change modal colors

--- swole-view/src/components/SwoleModal.vue
+++ swole-view/src/components/SwoleModal.vue
@@ -24,7 +24,7 @@
           <button
             @click="close"
             type="button"
-            class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+            class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-900 text-base font-medium text-white focus:outline-none sm:text-sm"
           >
             Close
           </button>
